1440ccf1fbe82391a51595f11441480272b61a81
ljhyeon <gee9328@gmail.com>
2025-01-06 08:40:39 +0700

    feat: add Descript component

---
+++ src/components/Descript.jsx
@@ -0,0 +1,27 @@
+// src/components/Descript.jsx
+import PropTypes from 'prop-types';
+import '../styles/Descript.css'
+
+const Descript = ({left, link, right, link_href, align='left' }) => {
+    return (
+        <div className={`descript ${align}`}>
+            {left && <span>{left}</span>}
+            {link && <a href={`${link_href}`}>{link}</a>}
+            {right && <span>{right}</span>}
+        </div>
+    )
+}
+
+Descript.propTypes = {
+    left: PropTypes.string,
+    link: PropTypes.string,
+    right: PropTypes.string,
+    link_href: PropTypes.string,
+    align: PropTypes.oneOf(['left', 'center', 'right']).isRequired,
+}
+
+Descript.defaultProps = {
+    align: 'left',
+}
+
+export default Descript;
