c7586544206e8b9a352f10cc0876aa50eec4af24
ljhyeon <gee9328@gmail.com>
2025-01-07 17:47:34 +0700

    feat: add logout

--- src/components/layouts/Content.jsx
+++ src/components/layouts/Content.jsx
@@ -1,13 +1,25 @@
 // src/components/Content.jsx
 
+import { useNavigate } from 'react-router-dom';
+
 import PropTypes from 'prop-types';
 import '../../styles/Content.css';
 
 const Content = ({route, children}) => {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+    
+        // redirect to login page
+        navigate('/');
+      };
+
     return (
         <main className="content">
             <header className="header">
                 <p>Dashboards / {route}</p>
+                <button onClick={handleLogout}>logout</button>
             </header>
             {children}
         </main>
