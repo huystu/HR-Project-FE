d281b8f7cc9e4b0f5aa32b794e751487319e5afe
ljhyeon <gee9328@gmail.com>
2025-01-07 08:40:14 +0700

    feat: make employee dashboard ui

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-// import App from './App.jsx'
-import Login from './pages/login.jsx'
+// import './index.css'
+import App from './App.jsx'
+// import Login from './pages/Login.jsx'
+// import EmployeePage from './pages/EmployeePage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    <Login />
+    <App />
+    {/* <Login /> */}
+    {/* <EmployeePage />  */}
   </StrictMode>,
 )
