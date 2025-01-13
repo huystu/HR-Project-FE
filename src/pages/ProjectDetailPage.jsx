ff6220da82a2b37a0cbd1dd6325120afaec4c34e
ljhyeon <gee9328@gmail.com>
2025-01-13 15:44:09 +0700

    feat: integrate Project Detail

--- src/pages/ProjectDetailPage.jsx
+++ src/pages/ProjectDetailPage.jsx
@@ -26,8 +26,17 @@ const ProjectDetailPage = () => {
     const { id } = useParams();
     const navigate = useNavigate();
 
+    const [loading, setLoading] = useState(false); // 로딩 상태
+
     const user = "Admin"; // User Name
+
+    // Project Info
+    const [projectInfo, setProjectInfo] = useState({});
+
+    // Members info
     const columns = ["Name", "Status", "Start Date", "End Date", "Email", "Role", "Action"];
+    const [members, setMembers] = useState([]);
+
 
     const roleOptions = [
         {value: "Team Leader", label: "Team Leader",},
@@ -38,32 +47,11 @@ const ProjectDetailPage = () => {
 
     const handleChange = (value) => {
         console.log(`selected ${value}`);
-      };
+    };
 
-    const data = [
-        {
-            Name: "Name1",
-            Status: "Doing",
-            "Start Date": "2024/12/30",
-            "End Date": "-",
-            Email: "aaa@aaa.com",
-            Role: <Select defaultValue="Team Manager" onChange={handleChange} options={roleOptions} />,
-            Action: ["Link", "Delete"],
-        },
-        {
-            Name: "Name2",
-            Status: "Exited",
-            "Start Date": "24/12/30",
-            "End Date": "25/01/05",
-            Email: "aaaa@aaa.com",
-            Role: <Select defaultValue="Designer" onChange={handleChange} options={roleOptions} />,
-            Action: ["Link", "Delete"],
-        },
-    ];
-
-    const [pageCount, setPageCount] = useState(1);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [pageSize] = useState(10);
+    // const [pageCount, setPageCount] = useState(1);
+    // const [currentPage, setCurrentPage] = useState(1);
+    // const [pageSize] = useState(10);
 
     const btnsArray = [
         <Button key="update">Update Project</Button>,
@@ -76,38 +64,98 @@ const ProjectDetailPage = () => {
     useEffect(() => {
         const token = localStorage.getItem("token");
         if (!token) {
-          navigate("/"); 
+            navigate("/"); 
         }
-      }, [navigate]);
+    }, [navigate]);
+
+    const formatPeriod = (startDate, endDate) => {
+        if (endDate) {
+            return `${startDate} - ${endDate}`;
+        }
+        else {
+            return `${startDate} - `;
+        }
+    };
+
+    // Fetch data from API
+    const fetchData = async () => {
+        setAuthToken(accessToken); // set the accessToken
+
+        setLoading(true);
+
+        try {
+            const response = await api.get(`/project/${id}/detail`);
+
+            if (response.status === 200) {
+                console.log("Success Project Detail: ", response);
+                console.log(response.data.data);
+
+                const projectInfo = response.data.data.projectInfo;
+
+                const formattedProjectData = {
+                    title: projectInfo.name,
+                    description: projectInfo.description,
+                    period: formatPeriod(projectInfo.startDate, projectInfo.endDate),
+                    status: projectInfo.status,
+                    id: projectInfo.id,
+                }
+
+                setProjectInfo(formattedProjectData);
+
+                const members = response.data.data.employees;
+
+                const formattedMembersData = members.map(member =>({
+                    Name: member.name,
+                    // Status: member.status,
+                    // "Start Date": member.startDate,
+                    // "End Date": member.endDate,
+                    Email: member.email,
+                    Role: <Select defaultValue={`${member.roleInProject}`} onChange={handleChange} options={roleOptions} />,
+                    Action: ['Link', 'Delete'],
+                    id: member.id,
+                }));
+                setMembers(formattedMembersData);
+            }
+        }
+        catch (error) {
+            console.error('Error fetching employee data:', error);
+        }
+
+        setLoading(false);
+    };
+
+    useEffect(() => {fetchData();}, []);
 
     return (
         // turn in into Project title
-        <Layout user={user} route={`Projects, ${id}`}> 
+        <Layout user={user} route={`Projects, ${projectInfo.title}`}>
+            {loading ? ( <LoadingSpinner /> ) // 로딩 중일 때 스피너 표시
+                : (
             <DashboardCard
-                header={`${id}`} // turn id into Project title
+                header={`${projectInfo.title}`}
                 btns={btnsArray}
-                progress_tag="In Progress"
-                date_tag="YYYY.MM.DD"
+                progress_tag={`${projectInfo.status}`}
+                date_tag={`${projectInfo.period}`}
             >
                 <div className="project-description">
                     Description
                     <div>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium ducimus voluptates perferendis molestias animi vitae, recusandae itaque ipsam accusamus qui officia necessitatibus, eum porro! Dolores repellat vero deleniti nam quisquam?
+                        {projectInfo.description}
                     </div>
                 </div>
                 <Tabs defaultActiveKey="1">
                     <Tabs.TabPane tab="Members" key="1">
                         <div style={{display: 'flex'}}>
-                            <Table columns={columns} data={data} />
+                            <Table columns={columns} data={members} />
                             {/* Add Members Button */}
                             <ImgButton>
                                 <IoMdPersonAdd />
                             </ImgButton>
                         </div>
-                        <Pagination currentPage={currentPage} pageCount={pageCount} onPageChange={setCurrentPage} pageSize={pageSize} />
+                        {/* <Pagination currentPage={currentPage} pageCount={pageCount} onPageChange={setCurrentPage} pageSize={pageSize} /> */}
                     </Tabs.TabPane>
                 </Tabs>
-            </DashboardCard>
+            </DashboardCard> )}
         </Layout>
     );
 };
