cb85c24bbd5d58eeaae8f26d9d816465f27c3265
ljhyeon <gee9328@gmail.com>
2025-01-15 11:11:06 +0700

    fix: Update skill display

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,11 +2,12 @@
 
 import PropTypes from "prop-types";
 import "../styles/Table.css";
-import Skill from "./Skill";
 import ImgButton from "./ImgButton";
 import { AiOutlineEdit, AiOutlineDelete } from "react-icons/ai"; //테이블에서 사용하는 편집 및 삭제 아이콘
 import { GrView } from "react-icons/gr";
 import { CiLink } from "react-icons/ci";
+
+import { Tag } from 'antd';
 
 //columns, data 기반으로 테이블 동적 렌더링
 //action 열의 편집 및 삭제 버튼, 사용자가 데이터 수정하거나 삭제
@@ -29,10 +30,10 @@ const Table = ({ columns, data, onEditClick, onDeleteClick, onViewClick, onLinkC
                   <>
                   {/*첫 두개의 기술만 표시하고 나머지 기술은 +x 형태로 개수 표시*/}
                     {row[column].slice(0, 2).map((skill, skillIndex) => (
-                      <Skill key={skillIndex} skill={skill} />
+                      <Tag color="blue" key={skillIndex}>{skill}</Tag>
                     ))}
                     {row[column].length > 2 && (
-                      <Skill skill={`+${row[column].length - 2}`} />
+                      <Tag color="blue">{`+${row[column].length - 2}`}</Tag>
                     )}
                   </>
                 ) : column === "Action" ? (
