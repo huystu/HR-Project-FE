24479bafb6e09994bbc999cbc0ba9e1dacdba8a4
ljhyeon <gee9328@gmail.com>
2025-01-20 15:09:16 +0700

    feat: create draft dashboard ui

---
+++ src/components/charts/LineGraph.jsx
@@ -0,0 +1,51 @@
+// src/components/charts/LineGraph.jsx
+import React from "react";
+import { Chart, } from "react-charts";
+import PropTypes from 'prop-types';
+
+
+const LineGraph = ({ title, data, height }) => {
+    
+    const primaryAxis = React.useMemo( () => (
+        { getValue: (datum) => datum.primary, }
+    ), [] );
+    
+    const secondaryAxes = React.useMemo( () => [
+        { getValue: (datum) => datum.secondary,
+            elementType: 'line',
+         },
+    ], [] );
+
+    return (
+        <>
+        {title}
+        <div style={{width: "100%", height: `${height}px`}}>
+            <Chart
+                options={{
+                    data,
+                    primaryAxis,
+                    secondaryAxes,
+                }}
+            />
+        </div>
+        </>
+    );
+};
+        
+LineGraph.propTypes = {
+    title: PropTypes.string,
+    data: PropTypes.arrayOf(
+        PropTypes.shape({
+            label: PropTypes.string.isRequired,
+            data: PropTypes.arrayOf(
+                PropTypes.shape({
+                    primary: PropTypes.string.isRequired,
+                    secondary: PropTypes.number.isRequired,
+                })
+            ).isRequired,
+        })
+    ).isRequired,
+    height: PropTypes.number, 
+}
+
+export default LineGraph;
--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,9 +1,14 @@
 // src/pages/DashboardPage.jsx
+import React from "react";
 import { useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
 
 import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons';
-import { Card, Statistic } from 'antd';
+import { Card, Statistic, Tabs } from 'antd';
+
+import LineGraph from "../components/charts/LineGraph";
+import DonutChart from "../components/charts/DonutChart";
+import BarGraph from "../components/charts/BarGraph";
 
 import api, { setAuthToken } from '../api';
 
@@ -49,11 +54,46 @@ const DashboardPage = () => {
 
         setLoading(false); // 정보 받아 오면 로딩 정지
     };
-    useEffect(() => {fetchData();}, []); // 컴포넌트가 마운트 될 때만 실행
+    // useEffect(() => {fetchData();}, []); // 컴포넌트가 마운트 될 때만 실행
+
+    const totalUserLineData = React.useMemo( () => [
+            {
+                label: 'This year',
+                data: [
+                    { primary: 'Jan', secondary: 65 },
+                    { primary: 'Feb', secondary: 59 },
+                    { primary: 'Mar', secondary: 80 },
+                    { primary: 'Apr', secondary: 81 },
+                    { primary: 'May', secondary: 56 },
+                    { primary: 'Jun', secondary: 55 },
+                    { primary: 'Jul', secondary: 40 },
+                ],
+            },
+            {
+                label: 'Last year',
+                data: [
+                    { primary: 'Jan', secondary: 28 },
+                    { primary: 'Feb', secondary: 48 },
+                    { primary: 'Mar', secondary: 40 },
+                    { primary: 'Apr', secondary: 19 },
+                    { primary: 'May', secondary: 86 },
+                    { primary: 'Jun', secondary: 27 },
+                    { primary: 'Jul', secondary: 90 },
+                ],
+            },
+        ], [] );
+
+    const employeeSkillsData = [
+        { name: 'Category A', value: 30 },
+        { name: 'Category B', value: 50 },
+        { name: 'Category C', value: 20 },
+    ];
+
+    const handleTabChange = (key) => { if (key !== '2') { console.log(key); } };
 
     return (
-        <Layout user={user} >
-            <div className="dashboard-cards">
+        <Layout user={user}>
+            <div className="dashboard-cards page-height-1">
                 <Card className="dashboard-card" bordered={false}>
                     <Statistic
                         title="Employees"
@@ -97,6 +137,26 @@ const DashboardPage = () => {
                     }}
                     suffix={<ArrowUpOutlined />}
                     />
+                </Card>
+            </div>
+            <div className="dashboard-cards page-height-2">
+                <Card className="dashboard-card-3">
+                    <Tabs defaultActiveKey="1" onChange={handleTabChange}>
+                        <Tabs.TabPane tab="Total Users" key="1">
+                            <LineGraph data={totalUserLineData} height={400}/>
+                        </Tabs.TabPane>
+                        <Tabs.TabPane tab="Total Projects" key="2">
+                            <LineGraph data={totalUserLineData} height={400}/>
+                        </Tabs.TabPane>
+                    </Tabs>
+                </Card>
+                <Card className="dashboard-card-2">
+                    <DonutChart title="Employee Skills Breakdown" data={employeeSkillsData} innerRadius={50} height={400} />
+                </Card>
+            </div>
+            <div className="dashboard-cards page-height-2">
+                <Card className="dashboard-card">
+                    <BarGraph title="Project Categories" data={totalUserLineData} height={400} />
                 </Card>
             </div>
         </Layout>
