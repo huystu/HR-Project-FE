5f27711844ef80868eba599721c4ef95bb203e32
ljhyeon <gee9328@gmail.com>
2025-01-22 17:49:20 +0700

    fix: fix the formik about email in login page

--- src/validationSchema.js
+++ src/validationSchema.js
@@ -3,6 +3,11 @@ import * as Yup from 'yup';
 export const validationSchema = Yup.object({
   email: Yup.string()
     .email('Please enter a vaild email address.')
+    .test('is-valid-email', 'Please enter a valid email address.', (value) => {
+      if (!value) return false;
+      const domainPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // 간단한 도메인 검사
+      return domainPattern.test(value);
+    })
     .required('Email is required.'),
   pw: Yup.string()
     .min(1, 'Password must be at least 1 characters long.')
